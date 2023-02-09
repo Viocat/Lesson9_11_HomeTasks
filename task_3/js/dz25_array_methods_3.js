95a2da2e5cb5b4b3e270b6ab5834cc1fd905ed58
Viocat <44540969+Viocat@users.noreply.github.com>
2023-02-09 19:15:33 +0200

    Update dz25_array_methods_3.js

--- task_3/js/dz25_array_methods_3.js
+++ task_3/js/dz25_array_methods_3.js
@@ -2,36 +2,6 @@
 
 const array = [1, 2, 3, 1, 2, 3, 1, 8, 3, 4, 8];
 
-// // Вам нужно написать функцию indexOf
-
-// function indexof(array, element, index = 0) {
-//     if (index >= array.length) return -1;
-//     if (index === -1) index = 0;
-//     for (index; index < array.length; index++) {
-//         if (array[index] === element) return index;
-//         if (index === array.length - 1) return -1;
-//     }
-
-// }
-
-// let result = indexof(array, 5, );
-// console.log(result);
-
-// //Вам нужно написать функцию lastIndexOf
-
-// function lastIndexof(array, element, index = array.length - 1) {
-//     if (index >= array.length) return -1;
-//     if (index < 0) index = array.length - 1;
-//     for (index; index >= 0; index--) {
-//         if (array[index] === element) return index;
-//         if (index === 0) return -1;
-//     }
-
-// }
-
-// result = lastIndexof(array, 3, );
-// console.log(result);
-
 //Вам нужно написать функцию find
 // function isFunction(functionToCheck)  {
 //     var getType = {};
